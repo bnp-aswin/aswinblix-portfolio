36cc425b8405d0d9793f5f80ccef9fee1010a1d7
@sw!n_bl!x <83761701+bnp-aswin@users.noreply.github.com>
2025-11-21 10:13:39 +0530

    Style fix

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -6,7 +6,7 @@ export const personalData = {
 		"My name is Aswin Blix. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
 	email: "aswinasvin13@gmail.com",
 	phone: "+91 9488 131 904",
-	address: "Kanniyakumari,Tamil Nadu, India - 629168",
+	address: "Kanniyakumari, Tamil Nadu, India - 629168",
 	github: "https://github.com/bnp-aswin",
 	facebook: "https://www.facebook.com/aswinblix/",
 	linkedIn: "https://www.linkedin.com/in/aswin-blix/",
