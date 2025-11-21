a174bd24dd423572a1572e94de070f4da76cb9b2
@sw!n_bl!x <83761701+bnp-aswin@users.noreply.github.com>
2025-11-21 07:46:00 +0530

    Fix more path

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -13,7 +13,7 @@ function Education() {
   return (
     <div id="education" className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
       <Image
-        src="/section.svg"
+        src="/aswinblix-portfolio/section.svg"
         alt="Hero"
         width={1572}
         height={795}
@@ -50,7 +50,7 @@ function Education() {
                   <GlowCard key={education.id} identifier={`education-${education.id}`}>
                     <div className="p-3 relative text-white">
                       <Image
-                        src="/blur-23.svg"
+                        src="/aswinblix-portfolio/blur-23.svg"
                         alt="Hero"
                         width={1080}
                         height={200}
--- app/components/homepage/experience/index.jsx
+++ app/components/homepage/experience/index.jsx
@@ -13,7 +13,7 @@ function Experience() {
   return (
     <div id="experience" className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
       <Image
-        src="/section.svg"
+        src="/aswinblix-portfolio/section.svg"
         alt="Hero"
         width={1572}
         height={795}
@@ -45,7 +45,7 @@ function Experience() {
                   <GlowCard key={experience.id} identifier={`experience-${experience.id}`}>
                     <div className="p-3 relative">
                       <Image
-                        src="/blur-23.svg"
+                        src="/aswinblix-portfolio/blur-23.svg"
                         alt="Hero"
                         width={1080}
                         height={200}
